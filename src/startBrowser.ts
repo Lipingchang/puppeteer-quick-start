c5136eb38904922e11043e29cc79b42b1e4a7d25
LPC <TGT@lalaland-2.local>
2019-12-26 20:04:15 +0800

    使用手动下载的chromium

--- src/startBrowser.ts
+++ src/startBrowser.ts
@@ -1,29 +1,45 @@
-import * as puppeteer from 'puppeteer'
+import { launch, LaunchOptions, Browser } from 'puppeteer';
+import path from 'path';
+import os from 'os';
 
-const proxyServer = 'http://127.0.0.1:1080'
+const proxyServer = "http://127.0.0.1:1087";
+const initViewPoint = { width: 900, height: 800 };
+const chromePath =
+  os.platform() === "darwin"
+    ? path.join(
+        path.dirname(__dirname),
+        "puppeteer.local-chromium",
+        "Chromium.app",
+        "Contents",
+        "MacOS",
+        "Chromium"
+      )
+    : path.join(path.dirname(__dirname), "puppeteer.local-chromium", "chrome.exe");
 
-const launchOptions: puppeteer.LaunchOptions = {
+const launchOptions: LaunchOptions = {
   headless: false,
-  args: [`--proxy-server=${proxyServer}`]
+  args: [`--proxy-server=${proxyServer}`],
+  executablePath: chromePath,
 }
 
 // const initViewPoint: Viewport = { width: 900, height: 800 }
 
-export async function doSomeThing (browser: puppeteer.Browser): Promise<void> {
+export async function doSomeThing (browser: Browser): Promise<void> {
   const page = await browser.newPage()
   await page.goto('http://www.baidu.com', { waitUntil: 'networkidle2' })
   await page.screenshot({ path: 'hh.png' })
 }
 
 async function main (): Promise<void> {
-  const browser = await puppeteer.launch(launchOptions)
+  console.log(launch)
+  const browser = await launch(launchOptions)
   // const page = await browser.newPage();
   // await page.setViewport(initViewPoint)
   // await page.goto('https://www.baidu.com', {waitUntil: 'networkidle2'});
   // await page.goto('http://www.google.com', {waitUntil: 'networkidle2'});
   // await page.screenshot({path: 'st.png'})
   // await page.pdf({path: 'hn.pdf', format: 'A4'});
-  // await doSomeThing(browser)
+  await doSomeThing(browser)
   await browser.close()
 }
 
